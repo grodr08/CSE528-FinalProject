abe9673c0f43856f2c091c06901f334f6786c77a
grodr08 <giancarlos.portales@outlook.com>
2025-03-26 03:27:24 -0400

    created bg, character prefab, parallax script, cinemachine cam

---
+++ CSE-Final-Project/Assets/Scripts/ParallexEffect.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ParallexEffect : MonoBehaviour
+{
+    public Camera cam;
+    public Transform followTarget;
+
+    // Starting Position for the parallax game object
+    Vector2 startingPosition;
+
+    // Start z value of the parallax game object
+    float startingZ;
+
+    // Distance that the camara has moved from the starting of the parallax object
+    Vector2 camMoveSinceStart => (Vector2)cam.transform.position - startingPosition;
+
+    float zDistanceFromTarget => transform.position.z - followTarget.transform.position.z;
+
+    // If object is in from of the target, use near clip plane. If behind target, use farClipPlane
+    float clippingPlane =>(cam.transform.position.z + (zDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+    // The further the object from the player, the faster the ParallaxEffect object will move. Drag it's Z value closer to the target to make it mvoe slower.
+    float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startingPosition = transform.position;
+        startingZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the target moves, move the parallax object the same distance times a multiplier
+        Vector2 newPosition = startingPosition + camMoveSinceStart * parallaxFactor;
+
+        // The X/Y position changes based on target travel speed times the parallax factor, but z stays consitent
+        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);
+    }
+}
